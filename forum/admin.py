c5020971131ab8e1680d2cbffd87886931500d86
CueChaotic <patrickbhammond@gmail.com>
2024-12-10 13:28:18 +0200

    Documentation added for admin.py in Forum app

--- forum/admin.py
+++ forum/admin.py
@@ -4,3 +4,10 @@ from .models import *
 # Register your models here.
 admin.site.register(Forum)
 admin.site.register(Discussion)
+
+"""The above are used to register the Forum and Discussion models, from the
+    'models.py' file, in the admin console of the site.
+
+    :param Forum: Pertains to the Forum model
+    :param Discussion: Pertains to the Discussion model
+"""
