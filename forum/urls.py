5eac0cf28717816a5b388af63b9f7c6986e45224
CueChaotic <patrickbhammond@gmail.com>
2024-12-10 15:13:43 +0200

    Added documentation for URL patterns in urls.py (in Forums app) for Sphinx utilisation

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'disc_forum'
+"""
+App name for the forum app. Used for easy referencing in templates.
+"""
 
 # URLs here:
 urlpatterns = [
@@ -9,4 +12,9 @@ urlpatterns = [
     path('addInForum/', views.addInForum, name='addInForum'),
     path('addInDiscussion/', views.addInDiscussion, name='addInDiscussion'),
 ]
-
+"""
+URLs for the forum app.
+The first path is the main forum page.
+The second path is for adding a new forum topic.
+The third path is for adding a new discussion/comment.
+"""
