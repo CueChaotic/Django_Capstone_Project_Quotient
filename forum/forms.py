2d79ac82d46c68949d38efda48db16f6a6e92df2
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 13:19:35 +0200

    Added Sphinx documentation for forms.py of the user reg app. Added in additional documentation in forms.py in the forum app.

--- forum/forms.py
+++ forum/forms.py
@@ -3,22 +3,33 @@ from .models import *
 
 # Forum and Discussion forms:
 class CreateInForum(ModelForm):
-    """Form that pulls in the Forum model and excludes the user field in the Meta
-    class.
-
-    :param ModelForm: Django form that is used to create a form from a model.
-    """
-    class Meta:
-        model = Forum
-        exclude = ["user"]
- 
-class CreateInDiscussion(ModelForm):
-    """Form that pulls in the Discussion model and excludes the user field in the
+    """Form that pulls in the Forum model and excludes the user field in the
     Meta class.
 
     :param ModelForm: Django form that is used to create a form from a model.
     """
     class Meta:
+        """Defines the model and excludes the user field.
+
+        :param model: Uses the Forum model from models.py in the forum app.
+        :param exclude: Excludes the user field from the form.
+        """
+        model = Forum
+        exclude = ["user"]
+
+
+class CreateInDiscussion(ModelForm):
+    """Form that pulls in the Discussion model and excludes the user field in
+    the Meta class.
+
+    :param ModelForm: Django form that is used to create a form from a model.
+    """
+    class Meta:
+        """Defines the model and excludes the user field.
+
+        :param model: Uses the Discussion model from models.py in the forum app.
+        :param exclude: Excludes the user field from the form.
+        """
         model = Discussion
         exclude = ["user"]
 
--- user_reg/forms.py
+++ user_reg/forms.py
@@ -4,16 +4,27 @@ from django.contrib.auth.models import User
 
 # User registration form class
 class UserRegistrationForm(UserCreationForm):
-    '''
-    Captures additional user information over and above the default fields
+    """Captures additional user information over and above the default fields
     provided by the UserCreationForm class.
-    '''
+
+    :param UserCreationForm: A form class that creates a new user.
+    :param first_name: A CharField that captures the user's first name.
+        Limited to 150 characters and is required.
+    :param last_name: A CharField that captures the user's last name.
+        Limited to 150 characters and is required.
+    :param email: An EmailField that captures the user's email address.
+        Limited to 150 characters and is required.
+    """
     first_name = forms.CharField(max_length=150, required=True)
     last_name = forms.CharField(max_length=150, required=True)
     email = forms.EmailField(max_length=150, required=True)
 
     class Meta:
+        """Defines the model and the fields to be used in the form.
+    
+        :param model: Uses the User model from the Django authentication system.
+        :param fields: A list of fields to be used in the form.
+        """
         model = User
         fields = ["first_name", "last_name", "email", "username", "password1",
                   "password2"]
-
