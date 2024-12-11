c2ec7b0f1c348ed9ee8e4b5cd35825200ae64c12
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 10:55:22 +0200

    Added Sphinx documentation for URL paths in urls.py for the front page app

--- front_page/urls.py
+++ front_page/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 app_name = 'front_page'
+"""
+App name for the forum app. Used for easy referencing in templates.
+"""
 
 # URLs here:
 urlpatterns = [
     path('', views.front_page, name='home_page')
 ]
 
+"""
+URLs for the front page.
+Only one path is needed for the front page.
+"""
