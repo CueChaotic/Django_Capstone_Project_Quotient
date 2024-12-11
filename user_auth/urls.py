da7ae121497aa4ffccf415a3ba926582ed681b67
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 11:41:16 +0200

    Added Sphinx documentation for urls.py of the user auth app.

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "user_auth"
+"""
+App name. Used for easy referencing in templates.
+"""
 
 urlpatterns = [
     path("", views.user_login, name = "login"),
@@ -10,4 +13,11 @@ urlpatterns = [
     path("logout/", views.logout_user, name = "logout"),
     path("logout_success/", views.logged_out, name = "logout_success"),
 ]
-
+"""
+URLs for the user authorisation app.
+The first path displays the initial login page.
+The second path is the user authentication function.
+The third path displays a success page on login.
+The fourth path is the user logout process.
+The final path displays a logout confirmation page.
+"""
