1a5046c20ca8c389f0547c21ba6fd803308fc581
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 11:11:23 +0200

    Added sphinx documentation for urls.py of the store page app. Amended documentation in urls.py of front_page for consistency and correctness

--- front_page/urls.py
+++ front_page/urls.py
@@ -3,15 +3,13 @@ from . import views
 
 app_name = 'front_page'
 """
-App name for the forum app. Used for easy referencing in templates.
+App name. Used for easy referencing in templates.
 """
 
 # URLs here:
 urlpatterns = [
     path('', views.front_page, name='home_page')
 ]
-
 """
-URLs for the front page.
-Only one path is needed for the front page.
+URL for the front page. Only one path is needed for the front page.
 """
--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 app_name = "store"
+"""
+App name. Used for easy referencing in templates.
+"""
 
+# URLs here:
 urlpatterns = [
     path("", views.store_page, name="store_page")
 ]
-
+"""
+URL for the store page. Only one path is needed.
+"""
