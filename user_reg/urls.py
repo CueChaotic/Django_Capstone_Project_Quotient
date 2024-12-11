91851434f3ecbad7853bed4bf86e7db009e8dcc9
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 13:24:11 +0200

    Added sphinx documentation for urls.py in the user_reg app.

--- user_reg/urls.py
+++ user_reg/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 app_name = "user_reg"
+"""App name. Used for easy referencing in templates.
+"""
 
 urlpatterns = [
     path("", views.register, name="register_user"),
     path("registration_success/", views.register_success, name="reg_success"),
 ]
-
+"""URLs for the user registration app.
+The first path displays the registration form.
+The second path displays a success message after registration after processing
+the form data.
+"""
