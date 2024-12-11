148c9a5761c903897cca830826d0ac9ae2d8c17c
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 13:38:29 +0200

    Added Sphinx documentation for views.py in user_reg app.

--- user_reg/views.py
+++ user_reg/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 
 # All Views
-
 def register(request):
-    '''
-    Checks form validity and saves the form data to the database.
-    Redirects to the success page if the form is valid.
-    '''
+    """Checks form validity and saves the form data to the database. Redirects
+    to the success page if the form is valid.
+
+    :param request: HTTP request object
+    :param form: An instance of the UserRegistrationForm class with data from
+        the POST request.
+
+    :return: Redirects to registration success view if the form is valid.
+        Otherwise returns the user to the new_user.html page to refill the
+        form.
+    """
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
@@ -18,9 +24,12 @@ def register(request):
     context = {"form": form}
     return render(request, "new_user.html", context)
 
-def register_success(request):
-    '''
-    Renders the success page.
-    '''
-    return render(request, "reg_success.html")
 
+def register_success(request):
+    """Renders the success page.
+
+    :param request: HTTP request object
+
+    :return: Returns the registration success page.
+    """
+    return render(request, "reg_success.html")
