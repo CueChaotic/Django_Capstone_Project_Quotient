f7d6e1073b1a1aa3f04afce801d4afb46c45bd5f
CueChaotic <patrickbhammond@gmail.com>
2024-12-11 11:00:30 +0200

    Added sphinx documentation for views.py of the front page app

--- front_page/views.py
+++ front_page/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # All views
 def front_page(request):
-    return render(request, 'front_page.html')
+    """
+    Renders the front (home) page.
 
+    :param request: The request object.
+    :param template_name: The name of the template to render.
+
+    :return: The rendered template.
+    """
+    return render(request, 'front_page.html')
